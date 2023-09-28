57a86aaea0d071453944d8d254ac0c312132eb6e
Amongusv21 <146339802+Amongusv21@users.noreply.github.com>
2023-09-28 11:14:06 +0000

    Init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+string imie;
+
+int main(){
+    cout << "Imie I nazwisko" << endl;
+    cin >> imie;
+    cout << "Nie podawaj swoich danych osobowych w internecie" << endl;
+    return 0;
+}
